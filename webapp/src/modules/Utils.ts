dfa34b465223b51f82fbc98946b9834b3ea3eab3
punarv pawade <punarvpawade17@gmail.com>
2023-11-07 02:11:23 +0530

    webapp/utils: revert dev change

--- webapp/src/modules/Utils.ts
+++ webapp/src/modules/Utils.ts
@@ -1,4 +1,4 @@
 export function getBaseUrl(): string {
-    // const { protocol, host } = window.location;
-    return `http://localhost:9080`;
-  }
+  const { protocol, host } = window.location;
+  return `${protocol}//${host}`;
+}
