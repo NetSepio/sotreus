4e50e607d2d5cbffa5257a23cab129c978f42cc3
Punarv Pawade <punarvpawade17@gmail.com>
2024-03-29 03:51:26 +0530

    webapp: use createBrowserRouter

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,22 +1,47 @@
 // App.tsx
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  BrowserRouter,
+  Route,
+  Routes,
+  RouterProvider,
+} from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import LandingPage from "./pages/LandingPage";
 import Server from "./pages/Server";
 import AuthComponent from "./components/Auth";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/auth",
+      element: <AuthComponent />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
+    },
+    {
+      path: "/server",
+      element: <Server />,
+    },
+  ]);
   return (
-    <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/auth" element={<AuthComponent />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/server" element={<Server />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    // <BrowserRouter>
+    //   <div>
+    //     <Routes>
+    //       <Route path="/" element={<LandingPage />} />
+    //       <Route path="/auth" element={<AuthComponent />} />
+    //       <Route path="/dashboard" element={<Dashboard />} />
+    //       <Route path="/server" element={<Server />} />
+    //     </Routes>
+    //   </div>
+    // </BrowserRouter>
   );
 };
 
